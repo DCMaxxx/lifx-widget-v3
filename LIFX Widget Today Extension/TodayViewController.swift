8ee3b1bfc090b2c78a0c1a3260b397a114c6bf87
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2017-02-15 11:30:09 +0100

    Cleaned up the project's skeleton

--- LIFX Widget Today Extension/TodayViewController.swift
+++ LIFX Widget Today Extension/TodayViewController.swift
@@ -11,16 +11,6 @@ import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
