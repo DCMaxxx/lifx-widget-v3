dc88922d5caa152a112d0a59f84d0c55dcafab57
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2017-02-15 11:20:37 +0100

    Added the project's skeleton

---
+++ LIFX Widget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LIFX Widget
+//
+//  Created by Maxime de Chalendar on 15/02/2017.
+//  Copyright © 2017 DCMaxxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
