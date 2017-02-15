257e34bfde49b4b8905a9b2b20d34933186bc7f2
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2017-02-15 15:44:16 +0100

    Themed the application

--- LIFX Widget/AppDelegate.swift
+++ LIFX Widget/AppDelegate.swift
@@ -7,10 +7,47 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        theme(application: application)
+    }
+
+}
+
+extension AppDelegate {
+
+    fileprivate func theme(application: UIApplication) {
+        let mainColor = UIColor(red: 35/255, green: 178/255, blue: 184/255, alpha: 1)
+
+        SVProgressHUD.setBackgroundColor(.white)
+        SVProgressHUD.setForegroundColor(mainColor)
+        SVProgressHUD.setDefaultMaskType(.black)
+        SVProgressHUD.setFont(.systemFont(ofSize: 14))
+
+        let navbarAppearance = UINavigationBar.appearance()
+        navbarAppearance.barTintColor = .black
+        navbarAppearance.tintColor = .white
+        navbarAppearance.titleTextAttributes = [ NSForegroundColorAttributeName: UIColor.white ]
+
+        UIToolbar.appearance().barTintColor = .black
+        let barButtonItemAppearance =  UIBarButtonItem.appearance()
+        barButtonItemAppearance.setTitleTextAttributes([
+            NSForegroundColorAttributeName: UIColor.white,
+            NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)], for: .normal)
+        barButtonItemAppearance.setTitleTextAttributes([
+            NSForegroundColorAttributeName: UIColor.darkGray,
+            NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)], for: .disabled)
+
+        UISlider.appearance().tintColor = mainColor
+        UIButton.appearance().tintColor = mainColor
+
+        application.statusBarStyle = .lightContent
+    }
 
 }
