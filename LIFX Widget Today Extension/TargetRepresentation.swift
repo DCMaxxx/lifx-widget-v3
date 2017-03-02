c4aac44891a40a36112573af8a683a03a3ae54f0
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2017-03-02 17:43:22 +0100

    Added the brightness of a color representation

--- LIFX Widget Today Extension/TargetRepresentation.swift
+++ LIFX Widget Today Extension/TargetRepresentation.swift
@@ -14,19 +14,28 @@ struct TargetRepresentation {
     let target: Target
     var isOn: Bool
     var currentColor: UIColor
+    var currentBrightnessImage: UIImage
 
     init(target: Target) {
         self.target = target
         self.isOn = false
         self.currentColor = .clear
+        self.currentBrightnessImage = #imageLiteral(resourceName: "brightness_medium")
     }
 
     init(target: Target, in lights: [LIFXLight]) {
         let targetedLights = TargetRepresentation.filter(lights: lights, for: target.identifier)
+        let availableLights = targetedLights.filter { $0.isConnected && $0.isOn }
 
         self.target = target
         self.isOn = targetedLights.reduce(false) { $0 || ($1.isConnected && $1.isOn) }
+
+        let colors = availableLights.map(Color.init)
         self.currentColor = colors.reduce(UIColor.clear) { $0.blend(with: $1.displayColor) }
+
+        let totalBrightness = availableLights.map { $0.brightness }.reduce(0, +)
+        let averageBrightness = totalBrightness / max(1, CGFloat(targetedLights.count))
+        self.currentBrightnessImage = Brightness(value: Float(averageBrightness)).icon
     }
 
     private static func filter(lights: [LIFXLight], for identifier: String) -> [LIFXLight] {
