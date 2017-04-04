e8d60b92a46e031aab06cf0fd17c3fc4074f26c1
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2017-04-04 22:47:31 +0200

    Added sample lights and targets for the unit tests, added a first unit test for the example

--- LIFX Widget Tests/TargetStatusesTests.swift
+++ LIFX Widget Tests/TargetStatusesTests.swift
@@ -7,17 +7,73 @@
 //
 
 import XCTest
+import LIFXAPIWrapper
+import SwiftyJSON
 
 class TargetStatusesTests: XCTestCase {
 
+    // MARK: - Properties
+    fileprivate var statuses: TargetsStatuses! // set in setUp()
+
+}
+
+// MARK: - Tests
+extension TargetStatusesTests {
+
+    func testSomething() {
+        print("Yes, it passes")
+    }
+
+}
+
+// MARK: - Setup, tear down
+extension TargetStatusesTests {
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        statuses = TargetsStatuses(targets: sampleTargets(), lights: sampleLights())
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        statuses = nil
+
         super.tearDown()
+    }
+
+    fileprivate func sampleLights() -> [LIFXLight] {
+        let json = parseJSONFile(named: "SampleLights")
+        let parsedLights = json.array?.flatMap { LIFXLight(dictionary: $0.dictionaryObject) }
+        guard let lights = parsedLights, !lights.isEmpty else {
+            fatalError("SampleLights.json doesn't contain any LIFXLight")
+        }
+
+        return lights
+    }
+
+    fileprivate func sampleTargets() -> [Target] {
+        let json = parseJSONFile(named: "SampleTargets")
+        let parsedTargets = json.array?.flatMap(Target.init)
+        guard let targets = parsedTargets, !targets.isEmpty else {
+            fatalError("SampleTargets.json doesn't contain any Target")
+        }
+        return targets
+    }
+
+    private func parseJSONFile(named name: String) -> JSON {
+        let bundle = Bundle(for: type(of: self))
+        guard let file = bundle.url(forResource: name, withExtension: "json") else {
+            fatalError("\(name).json not found")
+        }
+
+        let data: Data
+        do {
+            data = try Data(contentsOf: file)
+        } catch let error {
+            fatalError("Couldn't read file at \(file): \(error)")
+        }
+
+        return JSON(data: data)
     }
 
 }
