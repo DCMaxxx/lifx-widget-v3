e26fcbe1f58818e9e896d7a985210b4ab3312b78
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2017-04-04 21:24:10 +0200

    Added the tests target for the TargetStatuses class

---
+++ LIFX Widget Tests/TargetStatusesTests.swift
@@ -0,0 +1,23 @@
+//
+//  LIFX_Widget_Tests.swift
+//  LIFX Widget Tests
+//
+//  Created by Maxime de Chalendar on 04/04/2017.
+//  Copyright © 2017 DCMaxxx. All rights reserved.
+//
+
+import XCTest
+
+class TargetStatusesTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+}
